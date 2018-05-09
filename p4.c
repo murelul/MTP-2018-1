da761a898d85187d714c055c8bede2abe06b38b1
murelul <37636287+murelul@users.noreply.github.com>
2018-05-09 17:53:45 -0300

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,19 @@
+// Marcela Castellani 11721EBI026
+
+#include <stdio.h>
+
+int main(){
+	char string[100];
+	int i, j=0, string2[100];
+	printf("\nDigite um numero: ");
+	scanf("%s", &string);
+	printf("\nNumero como um inteiro: ");
+	for(i=0; string[i]!='\0'; i++){
+		if('0'<=string[i] && string[i]<='9'){
+			printf("%c", string[i]);
+			string2[j]=string[i];
+			j++;}
+		}
+	printf("\n\n\n");
+  return 0;
+}
