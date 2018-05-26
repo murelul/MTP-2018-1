0b8e226a3e65b888e3a249f0e198df977195a773
murelul <37636287+murelul@users.noreply.github.com>
2018-05-26 19:11:26 -0300

    Add files via upload

---
+++ p7 peretta.c
@@ -0,0 +1,24 @@
+//11721EBI026 Marcela Castellani
+
+#include <stdio.h>
+long int funcao_ackermann(int m,int n){
+	long int x;
+	if(m==0){return n+1;		
+	}
+	if(m>0 && n==0){return funcao_ackermann(m-1,1);
+	}
+	x=funcao_ackermann(m,n-1);
+	if(m>0 && n>0){return funcao_ackermann(m-1,x);
+	}
+}
+int main(){
+	int m, n;
+	printf("\nDigite um valor para m: ");
+	scanf("%d", &m);
+	getchar();
+	printf("\nDigite um valor para n: ");
+	scanf("%d", &n);
+	getchar();
+	printf("\n Valor resultante: %d", funcao_ackermann(m,n));
+return 0;
+}
