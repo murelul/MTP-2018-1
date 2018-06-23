a7deef131b08df64558392cdb7a55a2ee3fccb0a
murelul <37636287+murelul@users.noreply.github.com>
2018-06-23 13:58:00 -0300

    Update p4.c

--- p4.c
+++ p4.c
@@ -6,7 +6,7 @@ int main(){
 	char string[100];
 	int i, j=0, string2[100];
 	printf("\nDigite um numero: ");
-	scanf("%s", &string);
+	scanf("%s", string);
 	printf("\nNumero como um inteiro: ");
 	for(i=0; string[i]!='\0'; i++){
 		if('0'<=string[i] && string[i]<='9'){
